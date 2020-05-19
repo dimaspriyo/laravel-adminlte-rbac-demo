db3264bbec6d72122b6b7980cc4c4860b4feda56
dimaspriyo <dimspriyo@gmail.com>
2020-05-19 16:50:56 +0700

    Add Login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('adminlte/layout');
+    return redirect()->route('login.form');
 });
+Route::get('/login', 'HomeController@login')->name('login.form');
+Route::post('login', 'Auth\LoginController@login');
+Route::get('logout', 'Auth\LoginController@logout');
+
+Route::get('/home', 'HomeController@home')->name('home');
+
+
+
+//Auth::routes();
